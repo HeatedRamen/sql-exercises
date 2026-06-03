ffd4245c34a54811f23f9e0fac92fbca4db93a16
ramenmac <kevinnguyen4200@gmail.com>
2026-06-03 08:43:49 -0500

    Formatting?

--- Exercise4.sql
+++ Exercise4.sql
@@ -1,22 +1,30 @@
 use northwind;
 
-select  ProductName, UnitPrice
+select ProductName, UnitPrice
 from Products
 where UnitPrice = (select max(UnitPrice)
 						from Products);
                         
 select OrderID, ShipVia, ShipAddress
 from Orders
-where ShipVia in (select ShipperID from Shippers where CompanyName like "%Federal%");
+where ShipVia in (select ShipperID
+                  from Shippers
+                  where CompanyName like "%Federal%");
 
 select OrderID, ProductID
 from `Order Details`
-where ProductID in (select ProductID from Products where ProductName like "%Sasquatch%");
+where ProductID in (select ProductID
+                    from Products
+                    where ProductName like "%Sasquatch%");
 
 select FirstName, LastName, EmployeeID
 from Employees
-where EmployeeID in (select EmployeeID from Orders where OrderID = 10266); 
+where EmployeeID in (select EmployeeID
+                    from Orders
+                    where OrderID = 10266);
 
 select ContactName, CustomerID
 from Customers
-where CustomerID in (select CustomerID from Orders where OrderID = 10266);
+where CustomerID in (select CustomerID
+                    from Orders
+                    where OrderID = 10266);
